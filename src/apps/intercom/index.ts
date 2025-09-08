84714caf57b05402491b4ec23d20118b9789f24d
peterjaber <peterjaberau@gmail.com>
2025-09-08 14:04:15 +1000

    initial

---
+++ src/apps/intercom/index.ts
@@ -0,0 +1,83 @@
+const data = {
+    "app": {
+        "openConfig": {
+            "layout": "default",
+            "unifiedStyling": true,
+            "openTo": "home",
+            "spaces": [
+                {
+                    "type": "home",
+                    "label": "Home",
+                    "navigationLabel": "Home",
+                    "badge": null
+                },
+                {
+                    "type": "messages",
+                    "label": "Messages",
+                    "navigationLabel": "Messages",
+                    "badge": null
+                }
+            ],
+            "userHasReceivedChecklists": false,
+            "userHasLiveNewsfeed": false,
+            "userHasTickets": false
+        },
+        "selfServeSuggestionsMatch": false,
+        "name": "UI Bakery",
+        "features": {
+            "anonymousInboundMessages": true,
+            "googleAnalytics": false,
+            "hubspotInstalled": true,
+            "inboundMessages": true,
+            "marketoEnrichmentInstalled": false,
+            "googleAnalytics4Integration": true
+        },
+        "inboundConversationsDisabled": false,
+        "isDeveloperWorkspace": false,
+        "customGoogleAnalyticsTrackerId": null
+    },
+    "launcher": {
+        "isLauncherEnabled": true
+    },
+    "launcherDiscoveryMode": {
+        "hasDiscoveredLauncher": true
+    },
+    "launcherSettings": {
+        "alignment": "right",
+        "color": "#1E89EF",
+        "colorDark": "#1E89EF",
+        "hasRequiredFeatures": true,
+        "horizontalPadding": 20,
+        "instantBootEnabled": true,
+        "launcherLogoUrl": null,
+        "launcherLogoDarkUrl": null,
+        "secondaryColor": "#1E89EF",
+        "secondaryColorDark": "#1E89EF",
+        "showLauncher": true,
+        "themeMode": "light",
+        "updatedAt": 1751771520,
+        "verticalPadding": 40,
+        "isLoading": false
+    },
+    "user": {
+        "role": "visitor",
+        "locale": "en",
+        "hasConversations": false
+    },
+    "message": {},
+    "conversations": {
+        "byId": {}
+    },
+    "openOnBoot": {
+        "type": null,
+        "metadata": {},
+        "lastOpenAtTimestamp": 1751784649050
+    },
+    "operator": {
+        "lastComposerEvent": 0
+    },
+    "router": {
+        "isInitialized": false,
+        "navigationQueue": []
+    }
+}
---
+++ src/apps/meta-configurator/root.ts
@@ -0,0 +1,63 @@
+const rootContext = {
+    "fullPath": "/schema",
+    "path": "/schema",
+    "query": { },
+    "hash": "",
+    "name": "schema",
+    "params": { },
+    "matched": [
+        {
+            "path": "/schema",
+            "redirect": "__vue_devtool_undefined__",
+            "name": "schema",
+            "meta": {
+                "title": "SchemaEditor",
+                "sessionMode": "schemaEditor"
+            },
+            "beforeEnter": "__vue_devtool_undefined__",
+            "props": {
+                "default": false
+            },
+            "leaveGuards": {
+                "_custom": {
+                    "type": "set",
+                    "displayText": "Set[0]",
+                    "value": [ ],
+                    "readOnly": true
+                }
+            },
+            "updateGuards": {
+                "_custom": {
+                    "type": "set",
+                    "displayText": "Set[0]",
+                    "value": [ ],
+                    "readOnly": true
+                }
+            },
+            "enterCallbacks": { },
+            "components": {
+                "default": {
+                    "_custom": {
+                        "type": "component-definition",
+                        "displayText": "SchemaEditorView",
+                        "tooltipText": "Component definition",
+                        "file": "/Users/peterjaber/Desktop/Easyflow.io/Code/meta-configurator/meta_configurator/src/views/SchemaEditorView.vue"
+                    }
+                }
+            },
+            "__vd_id": "1"
+        }
+    ],
+    "meta": {
+        "title": "SchemaEditor",
+        "sessionMode": "schemaEditor"
+    },
+    "redirectedFrom": "__vue_devtool_undefined__",
+    "href": "/schema"
+}
+
+const captures = {
+    InitialSchemaSelectionDialog: [{"name":"Example Schema","key":"Example"},{"name":"From Json Schema Store","key":"JsonStore"},{"name":"Open Schema File","key":"File"},{"name":"Load Schema from URL","key":"URL"}]
+}
+
+
